9448e8a88a2d49d481b36c17372ee196fb1f257b
Shuo Wang <wshsdlz@cedar1.cedar.computecanada.ca>
2021-05-24 09:35:50 -0700

    divide the jobs, submitted the seceond part (SSI,SKW,KURT,AAC) with gpus

--- more_features/SSI_SKW_KURT_AAC.py
+++ more_features/SSI_SKW_KURT_AAC.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[7]:
+# In[1]:
 
 
 # from google.colab import drive
@@ -9,7 +9,7 @@
 # %cd /content/drive/My Drive/Colab Notebooks/Research/GazeSemgIntegration/more_features
 
 
-# In[1]:
+# In[3]:
 
 
 from IPython.core.interactiveshell import InteractiveShell
@@ -35,7 +35,7 @@ warnings.filterwarnings('ignore')
 
 # # Index variables
 
-# In[2]:
+# In[3]:
 
 
 subject_number = 1
@@ -43,7 +43,7 @@ subject_number = 1
 
 # # Find where the static data end from data file 'SXXX_dynamic.csv'
 
-# In[3]:
+# In[4]:
 
 
 static_end = []
@@ -58,7 +58,7 @@ for i in tqdm(range(10,10+subject_number)):
     
 
 
-# In[4]:
+# In[5]:
 
 
 static_end
@@ -66,7 +66,7 @@ static_end
 
 # # Analysis performance for different features
 
-# In[5]:
+# In[6]:
 
 
 # save accuracy in array 'acc_features'
@@ -75,104 +75,18 @@ acc_features = []
 name_features = []
 
 
-# ## STD--Standard deviation
+# ## SSI--Simple Square Integral
 
-# In[14]:
-
-
-# index variable
-'''change name, features'''
-name = 'STD'
-
-# start
-for i in tqdm(range(10,10+subject_number)):
-    # read csv files
-    current_emg = pd.read_csv('data_all_intact/S0' + str(i) + '_emg.csv', header=None)
-    current_grasp = pd.read_csv('data_all_intact/S0' + str(i) + '_grasp.csv', header=None)
-    current_grasprepetition = pd.read_csv('data_all_intact/S0' + str(i) + '_grasprepetition.csv', header=None)
-    
-    current_static_emg = current_emg.iloc[0:static_end[i-10]]
-    current_static_grasp = current_grasp.iloc[0:static_end[i-10]]
-    current_static_grasprepetition = current_grasprepetition.iloc[0:static_end[i-10]]
-    
-    # find raw train/test data
-    train_index = []
-    test_index = []
-    for j in range(len(current_static_grasprepetition)):
-        if current_static_grasprepetition.iloc[j,0] in [1,2,3,4,5,6,7,8,9]:
-            train_index.append(j)
-        if current_static_grasprepetition.iloc[j,0] in [10,11,12]:
-            test_index.append(j)
-    
-    trainx_raw = current_static_emg.iloc[train_index].reset_index(drop=True)
-    trainy_raw = current_static_grasp.iloc[train_index].reset_index(drop=True)
-
-    testx_raw = current_static_emg.iloc[test_index].reset_index(drop=True)
-    testy_raw = current_static_grasp.iloc[test_index].reset_index(drop=True)
-    
-    # add overlapping window to train/test data
-    ## 385 means the overlapping window is 200ms
-    ## train data
-    trainx = deepcopy(trainx_raw)
-    trainy = deepcopy(trainy_raw)
-    
-    for a in tqdm(range(385,len(trainx_raw))):
-        for b in range(0,12):
-            trainx.iloc[a,b] = trainx_raw.iloc[a-385:a+1,b].std()
-
-    trainx = trainx.iloc[385:len(trainx),:]
-    trainy = trainy.iloc[385:len(trainy),:]
-    
-    ## test data
-    testx = deepcopy(testx_raw)
-    testy = deepcopy(testy_raw)
-
-    for a in tqdm(range(385,len(testx_raw))):
-        for b in range(0,12):
-            testx.iloc[a,b] = testx_raw.iloc[a-385:a+1,b].std()
-
-    testx = testx.iloc[385:len(testx),:]
-    testy = testy.iloc[385:len(testy),:]
-    
-    # train/test model
-    lightGBM_model = ltb.LGBMClassifier()
-    lightGBM_model.fit(trainx,trainy)
-    expected1 = testy.values.flatten()
-    predicted1 = lightGBM_model.predict(testx)
-    counter = 0
-    for i in range(0, len(expected1)):
-        if expected1[i] == predicted1[i]:
-            counter += 1
-
-    accuracy1 = counter/len(expected1)
-
-acc_features.append(accuracy1)
-name_features.append(name)
+# In[36]:
 
 
-# In[17]:
-
-
-# trainx.to_csv('featured_data/'+name+'/trainx.csv')
-# trainy.to_csv('featured_data/'+name+'/trainy.csv')
-# testx.to_csv('featured_data/'+name+'/testx.csv')
-# testy.to_csv('featured_data/'+name+'/testy.csv')
-
-
-# ## RMS--Root Mean Square
-
-# In[18]:
-
-
-def rms(self):
+def ssi(self):
     current =0
-    square =0
+    square = 0
     for i in range(0, len(self)):
         current = self.iloc[i]
         square += current ** 2
-    mean = square/len(self)
-    root = math.sqrt(mean)
-    return root
+    return square
 
 
 # In[ ]:
@@ -181,7 +95,7 @@ def rms(self):
 # index variable
 '''change name, features'''
 
-name = 'RMS'
+name = 'SSI'
 
 # start
 for i in tqdm(range(10,10+subject_number)):
@@ -217,7 +131,7 @@ for i in tqdm(range(10,10+subject_number)):
     
     for a in tqdm(range(385,len(trainx_raw))):
         for b in range(0,12):
-            trainx.iloc[a,b] = rms(trainx_raw.iloc[a-385:a+1,b])
+            trainx.iloc[a,b] = ssi(trainx_raw.iloc[a-385:a+1,b])
 
     trainx = trainx.iloc[385:len(trainx),:]
     trainy = trainy.iloc[385:len(trainy),:]
@@ -228,7 +142,7 @@ for i in tqdm(range(10,10+subject_number)):
 
     for a in tqdm(range(385,len(testx_raw))):
         for b in range(0,12):
-            testx.iloc[a,b] = rms(testx_raw.iloc[a-385:a+1,b])
+            testx.iloc[a,b] = ssi(testx_raw.iloc[a-385:a+1,b])
 
     testx = testx.iloc[385:len(testx),:]
     testy = testy.iloc[385:len(testy),:]
@@ -247,137 +161,383 @@ for i in tqdm(range(10,10+subject_number)):
 
 acc_features.append(accuracy1)
 name_features.append(name)
-
-
-# In[ ]:
-
-
-# trainx.to_csv('featured_data/'+name+'/trainx.csv')
-# trainy.to_csv('featured_data/'+name+'/trainy.csv')
-# testx.to_csv('featured_data/'+name+'/testx.csv')
-# testy.to_csv('featured_data/'+name+'/testy.csv')
-
-
-# ## IEMG--Integrated EMG
-
-# In[6]:
-
-
-def iemg(self):
-    current =0
-    absolute =0
-    for i in range(0, len(self)):
-        current = self.iloc[i]
-        absolute += np.abs(current)
-    return absolute
-
-
-# In[ ]:
-
-
-# index variable
-'''change name, features'''
-
-name = 'IEMG'
-
-# start
-for i in tqdm(range(10,10+subject_number)):
-    # read csv files
-    current_emg = pd.read_csv('data_all_intact/S0' + str(i) + '_emg.csv', header=None)
-    current_grasp = pd.read_csv('data_all_intact/S0' + str(i) + '_grasp.csv', header=None)
-    current_grasprepetition = pd.read_csv('data_all_intact/S0' + str(i) + '_grasprepetition.csv', header=None)
-    
-    current_static_emg = current_emg.iloc[0:static_end[i-10]]
-    current_static_grasp = current_grasp.iloc[0:static_end[i-10]]
-    current_static_grasprepetition = current_grasprepetition.iloc[0:static_end[i-10]]
-    
-    # find raw train/test data
-    train_index = []
-    test_index = []
-    for j in range(len(current_static_grasprepetition)):
-        if current_static_grasprepetition.iloc[j,0] in [1,2,3,4,5,6,7,8,9]:
-            train_index.append(j)
-        if current_static_grasprepetition.iloc[j,0] in [10,11,12]:
-            test_index.append(j)
-    
-    trainx_raw = current_static_emg.iloc[train_index].reset_index(drop=True)
-    trainy_raw = current_static_grasp.iloc[train_index].reset_index(drop=True)
-
-    testx_raw = current_static_emg.iloc[test_index].reset_index(drop=True)
-    testy_raw = current_static_grasp.iloc[test_index].reset_index(drop=True)
-    
-    # add overlapping window to train/test data
-    ## 385 means the overlapping window is 200ms
-    ## train data
-    trainx = deepcopy(trainx_raw)
-    trainy = deepcopy(trainy_raw)
-    
-    for a in tqdm(range(385,len(trainx_raw))):
-        for b in range(0,12):
-            trainx.iloc[a,b] = iemg(trainx_raw.iloc[a-385:a+1,b])
-
-    trainx = trainx.iloc[385:len(trainx),:]
-    trainy = trainy.iloc[385:len(trainy),:]
-    
-    ## test data
-    testx = deepcopy(testx_raw)
-    testy = deepcopy(testy_raw)
-
-    for a in tqdm(range(385,len(testx_raw))):
-        for b in range(0,12):
-            testx.iloc[a,b] = iemg(testx_raw.iloc[a-385:a+1,b])
-
-    testx = testx.iloc[385:len(testx),:]
-    testy = testy.iloc[385:len(testy),:]
-    
-    # train/test model
-    lightGBM_model = ltb.LGBMClassifier()
-    lightGBM_model.fit(trainx,trainy)
-    expected1 = testy.values.flatten()
-    predicted1 = lightGBM_model.predict(testx)
-    counter = 0
-    for i in range(0, len(expected1)):
-        if expected1[i] == predicted1[i]:
-            counter += 1
-
-    accuracy1 = counter/len(expected1)
-
-acc_features.append(accuracy1)
-name_features.append(name)
-
-
-# In[ ]:
-
-
-# trainx.to_csv('featured_data/'+name+'/trainx.csv')
-# trainy.to_csv('featured_data/'+name+'/trainy.csv')
-# testx.to_csv('featured_data/'+name+'/testx.csv')
-# testy.to_csv('featured_data/'+name+'/testy.csv')
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
 
 
 # In[ ]:
 
 
 print(acc_features,name_features)
+trainx.to_csv('featured_data/'+name+'_trainx.csv')
+trainy.to_csv('featured_data/'+name+'_trainy.csv')
+testx.to_csv('featured_data/'+name+'_testx.csv')
+testy.to_csv('featured_data/'+name+'_testy.csv')
+
+
+# ## SKW--Skewness
+
+# In[66]:
+
+
+def skw(self):
+    current =0
+    result_sum = 0
+    for i in range(0, len(self)):
+        current = self.iloc[i]
+        result_sum += (current-self.mean())**3/len(self)/self.std()**3
+    return result_sum
 
 
 # In[ ]:
 
 
-acc_features
-name_features
+# index variable
+'''change name, features'''
+
+name = 'SKW'
+
+# start
+for i in tqdm(range(10,10+subject_number)):
+    # read csv files
+    current_emg = pd.read_csv('data_all_intact/S0' + str(i) + '_emg.csv', header=None)
+    current_grasp = pd.read_csv('data_all_intact/S0' + str(i) + '_grasp.csv', header=None)
+    current_grasprepetition = pd.read_csv('data_all_intact/S0' + str(i) + '_grasprepetition.csv', header=None)
+    
+    current_static_emg = current_emg.iloc[0:static_end[i-10]]
+    current_static_grasp = current_grasp.iloc[0:static_end[i-10]]
+    current_static_grasprepetition = current_grasprepetition.iloc[0:static_end[i-10]]
+    
+    # find raw train/test data
+    train_index = []
+    test_index = []
+    for j in range(len(current_static_grasprepetition)):
+        if current_static_grasprepetition.iloc[j,0] in [1,2,3,4,5,6,7,8,9]:
+            train_index.append(j)
+        if current_static_grasprepetition.iloc[j,0] in [10,11,12]:
+            test_index.append(j)
+    
+    trainx_raw = current_static_emg.iloc[train_index].reset_index(drop=True)
+    trainy_raw = current_static_grasp.iloc[train_index].reset_index(drop=True)
+
+    testx_raw = current_static_emg.iloc[test_index].reset_index(drop=True)
+    testy_raw = current_static_grasp.iloc[test_index].reset_index(drop=True)
+    
+    # add overlapping window to train/test data
+    ## 385 means the overlapping window is 200ms
+    ## train data
+    trainx = deepcopy(trainx_raw)
+    trainy = deepcopy(trainy_raw)
+    
+    for a in tqdm(range(385,len(trainx_raw))):
+        for b in range(0,12):
+            trainx.iloc[a,b] = skw(trainx_raw.iloc[a-385:a+1,b])
+
+    trainx = trainx.iloc[385:len(trainx),:]
+    trainy = trainy.iloc[385:len(trainy),:]
+    
+    ## test data
+    testx = deepcopy(testx_raw)
+    testy = deepcopy(testy_raw)
+
+    for a in tqdm(range(385,len(testx_raw))):
+        for b in range(0,12):
+            testx.iloc[a,b] = skw(testx_raw.iloc[a-385:a+1,b])
+
+    testx = testx.iloc[385:len(testx),:]
+    testy = testy.iloc[385:len(testy),:]
+    
+    # train/test model
+    lightGBM_model = ltb.LGBMClassifier()
+    lightGBM_model.fit(trainx,trainy)
+    expected1 = testy.values.flatten()
+    predicted1 = lightGBM_model.predict(testx)
+    counter = 0
+    for i in range(0, len(expected1)):
+        if expected1[i] == predicted1[i]:
+            counter += 1
+
+    accuracy1 = counter/len(expected1)
+
+acc_features.append(accuracy1)
+name_features.append(name)
+
+
+# In[68]:
+
+
+print(acc_features,name_features)
+trainx.to_csv('featured_data/'+name+'_trainx.csv')
+trainy.to_csv('featured_data/'+name+'_trainy.csv')
+testx.to_csv('featured_data/'+name+'_testx.csv')
+testy.to_csv('featured_data/'+name+'_testy.csv')
+
+
+# ## KURT--Kurtosis
+
+# In[69]:
+
+
+def kurt(self):
+    current =0
+    result_sum = 0
+    for i in range(0, len(self)):
+        current = self.iloc[i]
+        result_sum += (current-self.mean())**4/len(self)/self.std()**4
+    return result_sum
+
+
+# In[ ]:
+
+
+# index variable
+'''change name, features'''
+
+name = 'KURT'
+
+# start
+for i in tqdm(range(10,10+subject_number)):
+    # read csv files
+    current_emg = pd.read_csv('data_all_intact/S0' + str(i) + '_emg.csv', header=None)
+    current_grasp = pd.read_csv('data_all_intact/S0' + str(i) + '_grasp.csv', header=None)
+    current_grasprepetition = pd.read_csv('data_all_intact/S0' + str(i) + '_grasprepetition.csv', header=None)
+    
+    current_static_emg = current_emg.iloc[0:static_end[i-10]]
+    current_static_grasp = current_grasp.iloc[0:static_end[i-10]]
+    current_static_grasprepetition = current_grasprepetition.iloc[0:static_end[i-10]]
+    
+    # find raw train/test data
+    train_index = []
+    test_index = []
+    for j in range(len(current_static_grasprepetition)):
+        if current_static_grasprepetition.iloc[j,0] in [1,2,3,4,5,6,7,8,9]:
+            train_index.append(j)
+        if current_static_grasprepetition.iloc[j,0] in [10,11,12]:
+            test_index.append(j)
+    
+    trainx_raw = current_static_emg.iloc[train_index].reset_index(drop=True)
+    trainy_raw = current_static_grasp.iloc[train_index].reset_index(drop=True)
+
+    testx_raw = current_static_emg.iloc[test_index].reset_index(drop=True)
+    testy_raw = current_static_grasp.iloc[test_index].reset_index(drop=True)
+    
+    # add overlapping window to train/test data
+    ## 385 means the overlapping window is 200ms
+    ## train data
+    trainx = deepcopy(trainx_raw)
+    trainy = deepcopy(trainy_raw)
+    
+    for a in tqdm(range(385,len(trainx_raw))):
+        for b in range(0,12):
+            trainx.iloc[a,b] = kurt(trainx_raw.iloc[a-385:a+1,b])
+
+    trainx = trainx.iloc[385:len(trainx),:]
+    trainy = trainy.iloc[385:len(trainy),:]
+    
+    ## test data
+    testx = deepcopy(testx_raw)
+    testy = deepcopy(testy_raw)
+
+    for a in tqdm(range(385,len(testx_raw))):
+        for b in range(0,12):
+            testx.iloc[a,b] = kurt(testx_raw.iloc[a-385:a+1,b])
+
+    testx = testx.iloc[385:len(testx),:]
+    testy = testy.iloc[385:len(testy),:]
+    
+    # train/test model
+    lightGBM_model = ltb.LGBMClassifier()
+    lightGBM_model.fit(trainx,trainy)
+    expected1 = testy.values.flatten()
+    predicted1 = lightGBM_model.predict(testx)
+    counter = 0
+    for i in range(0, len(expected1)):
+        if expected1[i] == predicted1[i]:
+            counter += 1
+
+    accuracy1 = counter/len(expected1)
+
+acc_features.append(accuracy1)
+name_features.append(name)
+
+
+# In[ ]:
+
+
+print(acc_features,name_features)
+trainx.to_csv('featured_data/'+name+'_trainx.csv')
+trainy.to_csv('featured_data/'+name+'_trainy.csv')
+testx.to_csv('featured_data/'+name+'_testx.csv')
+testy.to_csv('featured_data/'+name+'_testy.csv')
+
+
+# ## AAC--Average Amplitude Change
+
+# In[1]:
+
+
+def aac(self):
+    current =0
+    diff =0
+    for i in range(1, len(self)):
+        current = self.iloc[i]
+        diff += np.abs(current - self.iloc[i-1])
+    return diff/len(self)
+
+
+# In[ ]:
+
+
+# index variable
+'''change name, features'''
+
+name = 'AAC'
+
+# start
+for i in tqdm(range(10,10+subject_number)):
+    # read csv files
+    current_emg = pd.read_csv('data_all_intact/S0' + str(i) + '_emg.csv', header=None)
+    current_grasp = pd.read_csv('data_all_intact/S0' + str(i) + '_grasp.csv', header=None)
+    current_grasprepetition = pd.read_csv('data_all_intact/S0' + str(i) + '_grasprepetition.csv', header=None)
+    
+    current_static_emg = current_emg.iloc[0:static_end[i-10]]
+    current_static_grasp = current_grasp.iloc[0:static_end[i-10]]
+    current_static_grasprepetition = current_grasprepetition.iloc[0:static_end[i-10]]
+    
+    # find raw train/test data
+    train_index = []
+    test_index = []
+    for j in range(len(current_static_grasprepetition)):
+        if current_static_grasprepetition.iloc[j,0] in [1,2,3,4,5,6,7,8,9]:
+            train_index.append(j)
+        if current_static_grasprepetition.iloc[j,0] in [10,11,12]:
+            test_index.append(j)
+    
+    trainx_raw = current_static_emg.iloc[train_index].reset_index(drop=True)
+    trainy_raw = current_static_grasp.iloc[train_index].reset_index(drop=True)
+
+    testx_raw = current_static_emg.iloc[test_index].reset_index(drop=True)
+    testy_raw = current_static_grasp.iloc[test_index].reset_index(drop=True)
+    
+    # add overlapping window to train/test data
+    ## 385 means the overlapping window is 200ms
+    ## train data
+    trainx = deepcopy(trainx_raw)
+    trainy = deepcopy(trainy_raw)
+    
+    for a in tqdm(range(385,len(trainx_raw))):
+        for b in range(0,12):
+            trainx.iloc[a,b] = aac(trainx_raw.iloc[a-385:a+1,b])
+
+    trainx = trainx.iloc[385:len(trainx),:]
+    trainy = trainy.iloc[385:len(trainy),:]
+    
+    ## test data
+    testx = deepcopy(testx_raw)
+    testy = deepcopy(testy_raw)
+
+    for a in tqdm(range(385,len(testx_raw))):
+        for b in range(0,12):
+            testx.iloc[a,b] = aac(testx_raw.iloc[a-385:a+1,b])
+
+    testx = testx.iloc[385:len(testx),:]
+    testy = testy.iloc[385:len(testy),:]
+    
+    # train/test model
+    lightGBM_model = ltb.LGBMClassifier()
+    lightGBM_model.fit(trainx,trainy)
+    expected1 = testy.values.flatten()
+    predicted1 = lightGBM_model.predict(testx)
+    counter = 0
+    for i in range(0, len(expected1)):
+        if expected1[i] == predicted1[i]:
+            counter += 1
+
+    accuracy1 = counter/len(expected1)
+
+acc_features.append(accuracy1)
+name_features.append(name)
+
+
+# In[ ]:
+
+
+print(acc_features,name_features)
+trainx.to_csv('featured_data/'+name+'_trainx.csv')
+trainy.to_csv('featured_data/'+name+'_trainy.csv')
+testx.to_csv('featured_data/'+name+'_testx.csv')
+testy.to_csv('featured_data/'+name+'_testy.csv')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+# print(acc_features,name_features)
+
+
+# In[14]:
+
+
+# df_acc_features = pd.DataFrame(acc_features)
+# df_name_features = pd.DataFrame(name_features)
+
+
+# In[ ]:
+
+
+# df_acc_features.to_csv('analysis_results/acc_features.csv')
+# df_name_features.to_csv('analysis_results/name_features.csv')
+
+
+# In[ ]:
+
+
+
+
+
+# In[12]:
+
+
+# # function test
+# dummy_data = {
+#     'signal': [1,2,3,4,5,6,7,8,9,-10]}
+
+# dummy_df = pd.DataFrame(dummy_data)
+# dummy_df
+
+
+# In[13]:
+
+
+# dasdv(dummy_df)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
 
 
 # In[ ]:
@@ -479,7 +639,7 @@ name_features
 # In[1]:
 
 
-# final test commit from Colab
+
 
 
 # In[ ]:
--- more_features/SSI_SKW_KURT_AAC.sh
+++ more_features/SSI_SKW_KURT_AAC.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=6
+#SBATCH --gres=gpu:2
 #SBATCH --mem=32000M
-#SBATCH --time=0-12:00
+#SBATCH --time=10-0:00
 
 source ~/GazeSemgIntegration/bin/activate
-python features_analysis.py
+python SSI_SKW_KURT_AAC.py
