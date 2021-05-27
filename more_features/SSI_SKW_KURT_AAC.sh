88bb2ba58c7f54ec9bf74c6b228dc931a51b0b03
MountStonne <shuow@mun.ca>
2021-05-27 02:37:39 +0000

    tiny code files

--- more_features/SSI_SKW_KURT_AAC.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH --cpus-per-task=6
-#SBATCH --gres=gpu:2
-#SBATCH --mem=32000M
-#SBATCH --time=10-0:00
-
-source ~/GazeSemgIntegration/bin/activate
-python SSI_SKW_KURT_AAC.py
--- more_features/features_analysis2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#SBATCH --cpus-per-task=12
-#SBATCH --mem=32000M
-#SBATCH --time=10-0:00
-
-source ~/GazeSemgIntegration/bin/activate
-python features_analysis2.py
