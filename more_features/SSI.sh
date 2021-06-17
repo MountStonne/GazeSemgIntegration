9565309513ca97b51389bd5f9f05be77c711ff67
MountStonne <shuow@mun.ca>
2021-06-17 16:22:11 +0000

    fix a small typo bug

--- more_features/SSI.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH --time=0-14:00
-#SBATCH --cpus-per-task=8
-#SBATCH --gres=gpu:1
-#SBATCH --mem=4096M
-
-source ~/GazeSemgIntegration/bin/activate
-python SSI.py
--- more_features/useless_code/test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#SBATCH --time=00:15:00
-#SBATCH --#!/bin/bash
-
-echo 'Hello, world!'
